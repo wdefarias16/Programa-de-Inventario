aad6423809917d7c1a7a911c2e8b3e64caddf21e
wdefarias16 <wdefarias16@gmail.com>
2025-04-10 17:13:28 -0400

    antes de descargar

--- Inicio.py
+++ Inicio.py
@@ -5,7 +5,7 @@ from threading import Timer
 
 fonts = [('Roboto light',25),('Roboto light',15)]
 app_colors = ['#eaeaea','#1d1d1d','#1c9bac','#166c78','#5d5d5d']
-lightmode = True
+
 
 
 class Inicio(ctk.CTkFrame):
